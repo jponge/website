83d72b8846982628488cfd300ac59bb281d22a8d
Julien Ponge <julien.ponge@gmail.com>
2019-05-03 11:33:01 +0200

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ bundle exec jekyll build
 
 echo "\n⚙️  Copying to gh-pages..."
 rm -rf ${GHP_DIR}
-git clone --branch gh-pages . ${GHP_DIR}
+git clone --depth=1 --branch gh-pages $REPO ${GHP_DIR}
 rm -rf ${GHP_DIR}/*
 cp -R _site/* ${GHP_DIR}
 cd ${GHP_DIR}
