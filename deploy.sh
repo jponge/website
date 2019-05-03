0e9d85135bb6f484aaff4052cfda7d1f84d74f76
Julien Ponge <julien.ponge@gmail.com>
2019-05-03 11:15:15 +0200

    Full deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,18 @@ set -ea
 GHP_DIR=_gh-pages
 REPO=git@github.com:jponge/website.git
 
-echo "⚙️ Building..."
+echo "⚙️  Building..."
 bundle exec jekyll clean
 bundle exec jekyll build
 
-echo "⚙️ Copying to gh-pages..."
+echo "\n⚙️  Copying to gh-pages..."
 rm -rf ${GHP_DIR}
-git clone --depth=1 --branch gh-pages . ${GHP_DIR}
+git clone --branch gh-pages . ${GHP_DIR}
 rm -rf ${GHP_DIR}/*
 cp -R _site/* ${GHP_DIR}
 cd ${GHP_DIR}
 git add -A
+git commit -m "New build"
+
+echo "\n️🚀   Push..."
+git push $REPO gh-pages
